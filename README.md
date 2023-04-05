8dfc37a68497f110ed9b807d7c12953d06288b51
Olebogeng Sebogodi <sebogodio@icloud.com>
2023-04-05 22:18:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# co6e94e57b43dbadf8ba8e787
+
+Quick start:
+
+```
+$ npm install
+$ npm start
+````
+
+Head over to https://vitejs.dev/ to learn more about using vite
+
---
+++ index.js
@@ -0,0 +1,84 @@
+import { tweetsData } from "./data.js";
+const tweetInput = document.getElementById("tweet-input")
+const tweetBtn = document.getElementById("tweet-btn")
+
+tweetBtn.addEventListener('click', function(){
+    
+})
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.likes){
+        handleLikeClick(e.target.dataset.likes)
+    }else if(e.target.dataset.retweets){
+        handleRetweetClick(e.target.dataset.retweets)
+    }
+})
+
+function handleLikeClick(tweetId){
+    
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    if(targetTweetObj.isLiked){
+        targetTweetObj.likes--
+    }else{
+        targetTweetObj.likes++
+    }
+
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+    render()
+}
+
+function handleRetweetClick(tweetId){
+
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    render()
+}
+
+function getFeedHtml(){
+    let feedHtml = ``
+    tweetsData.forEach(function(tweet){
+        feedHtml += 
+        `<div class="tweet">
+            <div class="tweet-inner">
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                            <i class="fa-regular fa-comment-dots" data-replies="${tweet.uuid}"></i>
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-heart" data-likes="${tweet.uuid}"></i>
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-retweet" data-retweets="${tweet.uuid}"></i>
+                            ${tweet.retweets}
+                        </span>
+                    </div>   
+                </div>            
+            </div>
+        </div>`
+    })
+    return feedHtml
+}
+
+function render(){
+    const feed = document.getElementById("feed")
+    feed.innerHTML = getFeedHtml()
+}
+
+render()
