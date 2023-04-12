26552e2ec4f44a251109f6065007070788768662
Olebogeng Sebogodi <sebogodio@icloud.com>
2023-04-12 12:36:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { tweetsData } from "./data.js";
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
-const tweetInput = document.getElementById("tweet-input")
+
 
 
 document.addEventListener('click', function(e){
@@ -52,19 +52,23 @@ function handleReplyClick(replyId){
 }
 
 function handleTweetBtnClick(){
-    tweetsData.unshift(         
-        {
-            handle: `@Scrimba`,
-            profilePic: `images/scrimbalogo.png`,
-            likes: 0,
-            retweets: 0,
-            tweetText: tweetInput.value,
-            replies: [],
-            isLiked: false,
-            isRetweeted: false,
-            uuid: uuidv4()
-        })
-        render()
+    const tweetInput = document.getElementById("tweet-input")
+    if(tweetInput.value){
+        tweetsData.unshift(         
+            {
+                handle: `@Scrimba`,
+                profilePic: `images/scrimbalogo.png`,
+                likes: 0,
+                retweets: 0,
+                tweetText: tweetInput.value,
+                replies: [],
+                isLiked: false,
+                isRetweeted: false,
+                uuid: uuidv4()
+            })
+            render()
+            tweetInput.value = ''
+    }
 }
 
 function getFeedHtml(){
